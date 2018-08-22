91a4af91d6bcbb896e0c1a1c9df45e7597fbf794
longweiqiang <573925242@qq.com>
2018-08-22 23:30:20 +0800

    第一次提交

---
+++ polls/urls.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+# @Time       : 2018/8/22 22:45
+# @Author     : Weiqiang.long
+# @File       : urls.py.py
+# @Software   : PyCharm
+# @Description: 
+# @TODO       :
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index')
+]
