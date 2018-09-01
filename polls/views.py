0a7aec715924b99fa8e7bcac4eaa98b836a0fbc4
longweiqiang <573925242@qq.com>
2018-09-01 22:27:53 +0800

    增加tutorial03练习代码

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.template import loader
+
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+    # template = loader.get_template('polls/index.html')
+    # context = {
+    #     'latest_question_list':latest_question_list
+    # }
+    # return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question {0}.".format(question_id))
-
+    # return HttpResponse("You're looking at question {0}.".format(question_id))
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    ## 尝试用 get() 函数获取一个对象，如果不存在就抛出 Http404 错误
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question':question_id})
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
